35c93e74ca2fb2d86db7efde403fc96e5fb6b21d
Vladislav36366 <mihailo04@mail.ru>
2024-09-08 00:58:59 +0500

    Update Дополнительное практическое задание по модулю.py

--- Дополнительное практическое задание по модулю.py
+++ Дополнительное практическое задание по модулю.py
@@ -1,8 +1,6 @@
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21]
-entered_number = int(input('Введите число от 3 до 20'))
+entered_number = int(input('Введите число от 3 до 20: '))
 key_door = []
-#if entered_number == 20:
-    #entered_number = entered_number - 1
 for i in range(entered_number ,numbers[entered_number]):
     for j in range(1, len(numbers)+1):
        for l in range(1,len(numbers)+1):
