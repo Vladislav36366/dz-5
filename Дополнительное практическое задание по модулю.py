ec4659e670bc6130284969ea29ff0fbadbd90926
Vladislav36366 <mihailo04@mail.ru>
2024-09-09 17:09:21 +0500

    Update Дополнительное практическое задание по модулю.py

--- Дополнительное практическое задание по модулю.py
+++ Дополнительное практическое задание по модулю.py
@@ -2,16 +2,24 @@ numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20
 entered_number = int(input('Введите число от 3 до 20: '))
 key_door = []
 for i in range(entered_number ,numbers[entered_number]):
-    for j in range(1, len(numbers)+1):
-       for l in range(1,len(numbers)+1):
+    for j in range(1, len(numbers)):
+       for l in range(1,len(numbers)):
            sum_ = j + l
            if i % sum_ == 0:
                key_door.append(j)
                key_door.append(l)
-    a = key_door[:len(key_door)//2]
-    b = key_door[len(key_door)//2:]
+    if entered_number == 8:
+        a = key_door[:len(key_door) // 2 + 1]
+        b = key_door[len(key_door) // 2:]
+    if entered_number == 18:
+        a = key_door[:len(key_door) // 2 + 1]
+        b = key_door[len(key_door) // 2:]
+    if entered_number != 8 and entered_number != 18:
+        a = key_door[:len(key_door)//2]
+        b = key_door[len(key_door)//2:]
     print(i , a)
     key_door = []
+
 
 
 
